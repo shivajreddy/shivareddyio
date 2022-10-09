6e78f57e9120e24dac480bd8078a6372ba519393
ShivaSMBP <shivajayaramireddy@gmail.com>
2022-10-09 19:02:18 -0400

    navbar fixed margin, mobile size

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,36 +4,37 @@ import styles from "./NavBar.module.css";
 
 function NavBar() {
   return (
-    <div>
+      <div className="nav-container">
+        <div className="nav-sub-container">
 
-      <nav className="primary-header flex">
-        <a href="#" className="text-2xl text-indigo-700 pr-40">Shiva Reddy</a>
-        <ul id="primary-navigation" className="primary-navigation flex">
+          <nav >
+          <a href="#" className="text-2xl text-indigo-700">Shiva Reddy</a>
+          <ul id="primary-navigation" className="primary-navigation flex">
 
+            <li>
+              <a className="uppercase letter-spacing-2" href="#">
+                <span aria-hidden="true">00</span>
+                Blog
+              </a>
+            </li>
 
-          <li>
-            <a className="uppercase letter-spacing-2" href="#">
-              <span aria-hidden="true">00</span>
-              Blog
-            </a>
-          </li>
+            <li>
+              <a className="uppercase letter-spacing-2" href="#">
+                <span aria-hidden="true">00</span>
+                Projects
+              </a>
+            </li>
 
-          <li>
-            <a className="uppercase letter-spacing-2" href="#">
-              <span aria-hidden="true">00</span>
-              Projects
-            </a>
-          </li>
+            <li>
+              <a className="uppercase letter-spacing-2" href="#">
+                <span aria-hidden="true">00</span>
+                Fun-facts
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-          <li>
-            <a className="uppercase letter-spacing-2" href="#">
-              <span aria-hidden="true">00</span>
-              Fun-facts
-            </a>
-          </li>
-        </ul>
-      </nav>
-
+      </div>
     </div>
   );
 }
